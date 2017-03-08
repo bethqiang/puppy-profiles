4511e46834a59e3802de947fd7540d4b8f543844
Beth Qiang <bethany.qiang@gmail.com>
2017-03-07 23:20:57 -0600

    Add get one user & edit one user routes

--- server/users.js
+++ server/users.js
@@ -9,7 +9,28 @@ router.get('/', (req, res, next) => {
 });
 
 // Get one user
+router.get('/:id', (req, res, next) => {
+  User.findById(req.params.id)
+  .then(user => res.json(user))
+  .catch(next);
+});
 
-// Edit your user page
+// Edit your (and only your) user page
+router.post('/:id', (req, res, next) => {
+  // Will implement this conditional when Passport/login is set up
+  // if (req.user.id === req.params.id) {
+  User.findById(req.params.id)
+  .then(user => {
+    user.email = req.body.email || user.email;
+    user.password = req.body.password || user.password;
+    user.name = req.body.name || user.name;
+    user.picture = req.body.picture || user.picture;
+    user.description = req.body.description || user.description;
+    return user.save();
+  })
+  .then(user => res.json(user))
+  .catch(next);
+  // }
+});
 
 module.exports = router;
