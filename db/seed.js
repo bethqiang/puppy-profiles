0360e555fb65f269fcb1c924859916d9ba42f2ac
Beth Qiang <bethany.qiang@gmail.com>
2017-03-08 21:44:29 -0600

    Clean up BE

--- db/seed.js
+++ db/seed.js
@@ -3,7 +3,7 @@ const chalk = require('chalk');
 const User = require('./models');
 
 const users = [
-  { email: 'chalupa@apa.org', password: '1234', name: 'Chalupa', picture: '/img/chalupa.png', description: '' },
+  { email: 'chalupa@apa.org', password: '1234', name: 'Chalupa', picture: '/img/chalupa.png', description: 'This happy young adult is a real people-pleaser, and would love to share a home with a fun active family.' },
   { email: 'cosmo@apa.org', password: '1234', name: 'Cosmo', picture: '/img/cosmo.png', description: 'If fetching and getting belly rubs were Olympic events, there\'s no question that I would bring home gold.' },
   { email: 'dodger@apa.org', password: '1234', name: 'Dodger', picture: '/img/dodger.png', description: 'I enjoy the finer things in life: treats, lounging with his people, sunbathing and doggie massages.' },
   { email: 'east@apa.org', password: '1234', name: 'East', picture: '/img/east.jpg', description: 'I\'m a pup who loves to be curled up next to you, whether it\'s up against your leg or in your lap.' },
@@ -14,4 +14,8 @@ const users = [
   { email: 'trooper@apa.org', password: '1234', name: 'Trooper', picture: '/img/trooper.jpg', description: 'I\'m an affectionate young guy who\'s always happy to see people, especially when they give me belly rubs!' }
 ];
 
-User.create(users, () => console.log(chalk.green(`--- Seeded ${users.length} pups successfully. ---`)));
+User.remove()
+.then(() => User.create(users, () => {
+  console.log(chalk.green(`--- Seeded ${users.length} pups successfully. ---`));
+  process.exit();
+}));
--- server/routes/users.js
+++ server/routes/users.js
@@ -18,20 +18,19 @@ router.get('/:name', (req, res, next) => {
 
 // Edit your (and only your) user page
 router.put('/:name', (req, res, next) => {
-  // Will implement this conditional when Passport/login is set up
-  // if (req.user.name === req.params.name) {
-  User.findOne({ name: req.params.name }).exec()
-  .then(user => {
-    user.email = req.body.email || user.email;
-    user.password = req.body.password || user.password;
-    user.name = req.body.name || user.name;
-    user.picture = req.body.picture || user.picture;
-    user.description = req.body.description || user.description;
-    return user.save();
-  })
-  .then(user => res.json(user))
-  .catch(next);
-  // }
+  if (req.user.name === req.params.name) {
+    User.findOne({ name: req.params.name }).exec()
+    .then(user => {
+      user.email = req.body.email || user.email;
+      user.password = req.body.password || user.password;
+      user.name = req.body.name || user.name;
+      user.picture = req.body.picture || user.picture;
+      user.description = req.body.description || user.description;
+      return user.save();
+    })
+    .then(user => res.json(user))
+    .catch(next);
+  }
 });
 
 module.exports = router;
